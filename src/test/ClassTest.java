4c0b297407a001243cb8342f8fe437d33cb8cf0f
Nicolas <Nicolas@DESKTOP-7A4U4C3.home>
2023-12-17 10:58:23 +0100

    Commit Test Branche

--- src/test/ClassTest.java
+++ src/test/ClassTest.java
@@ -5,7 +5,12 @@ public class ClassTest {
 	public static void main(String[] args) {
 
 		System.out.println("Test Git Branche");
+		TestBranche();
 
+	}
+	
+	public static void TestBranche() {
+		System.out.println("Branche Test");
 	}
 
 }
